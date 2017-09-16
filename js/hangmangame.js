c79e130812eb5166483d7e436bc54cd450b7efed
Tomek <margin-zero@atcplanet.com>
2017-09-16 22:24:36 +0200

    W.I.P.

--- js/hangmangame.js
+++ js/hangmangame.js
@@ -2,6 +2,8 @@
 
 var questions,
     categories,
+    current_questions = [],
+    question_id = 0,
     category_id = -1,
     questions_count = 0,
     errors_left = 5,
@@ -41,20 +43,11 @@ function HangManGameRun() {
 
 function HangManGameInit() {
 
-    category_id = -1;
-    questions_count = 0;
-    errors_left = 5;
-
     // remove answer tiles
     $(".game-table").empty();
 
     // remove .clicked class from letters
     $(".game-letters>div.clicked").removeClass("clicked");
-
-    // reset score variables
-    score_total = 0;
-    score_won = 0;
-    score_lost = 0;
 
     //reset game status
     game_in_progress = false;
@@ -62,14 +55,17 @@ function HangManGameInit() {
 
 
 function HangManGameRefreshInfo() {
-    $(".game-info>.left>p>a").text(categories[category_id]);
+    if (category_id>=0) {
+        $(".game-info>.left>p>a").text(categories[category_id] + " - (" + current_questions.length +")");
+    };
+    
     $(".game-info>.right>p>span").text(errors_left);
     
     $(".game-score span.total").text(score_total);
     $(".game-score span.won").text(score_won);
     $(".game-score span.lost").text(score_lost);
 
-    if (category_id >= 0) {
+    if ((question_id) < questions_count) {
         $(".game-buttons>a").show();
     }
     else
@@ -85,11 +81,17 @@ function HangManGameEventFunctions() {
     
     // click on category name - game screen
     $(".game-info>.left a").click(function() {
-        GameCategoryClick($(this));
+        GameCategoryClick();
     });
 
+    // click on category name - category list
     $(".category-list-container>a").click(function() {
         CategoryListClick($(this));
+    })
+
+    // click on "new game" button
+    $(".game-buttons>a").click(function() {
+        NewGameClick($(this));
     })
 }
 
@@ -97,22 +99,85 @@ function HangManGameEventFunctions() {
 
 // definitions of object events functions
 
-function GameCategoryClick($this) {
+function GameCategoryClick() {
     $(".category-list").show();
 }
 
 function CategoryListClick($this) {
+
     if (category_id != $($this).index()-1) {
-        
+        current_questions = [];
         category_id = $($this).index()-1;
-        if (game_in_progress) {
-            score_total += 1;
-            score_lost += 1;
-            game_in_progress = false;
-        };
+        score_total = 0;
+        score_lost = 0;
+        score_won = 0;
+        game_in_progress = false;
+        
         errors_left = 5;
+        question_id = 0;
+        questions_count = 0;
+
+        $.each(questions, function( index, value ) {
+
+            if (value.category==categories[category_id]) {
+                questions_count += 1;
+                current_questions.push(value.question.toUpperCase());
+            };
+        });  
+
+        // let's toss questions
+        let i,id1,id2,tmp;
+
+        for (i=0;i<100;i++) {
+            id1 = randomNumber(0,current_questions.length-1);
+            id2 = randomNumber(0,current_questions.length-1);
+            tmp = current_questions[id1];
+            current_questions[id1] = current_questions[id2];
+            current_questions[id2] = tmp;
+        };
     };
+    
     $(".category-list").hide();
 
+    HangManGameInit();
     HangManGameRefreshInfo();
+}
+
+function NewGameClick($this) {
+    if (game_in_progress) { 
+        score_total += 1;
+        score_lost += 1;
+    };
+    HangManGameInit();
+    game_in_progress = true;
+
+    GameInitQuestion();
+    HangManGameRefreshInfo();
+}
+
+function GameInitQuestion() {
+    var question = current_questions[question_id],
+        i;
+
+    alert(question + "   questionID: "+question_id+"   questions.length="+current_questions.length);
+
+    for (i=0; i<current_questions[question_id].length; i++) {
+        if (current_questions[question_id].charAt(i) != " ") {
+            $(".game-table").append("<div></div>");
+        } 
+        else {
+            $(".game-table").append("<div class='space'></div>");
+        };
+
+    };
+    question_id +=1;
+}
+
+
+
+
+// generic functions
+
+function randomNumber(start, end) {
+    return Math.floor(Math.random() * (end - start + 1) + start);
 }
